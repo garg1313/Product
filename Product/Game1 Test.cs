0e38645d15fa56d7f1a6b1927fdbc105b4cfc732
Jatin Garg <Jatin.rex797@gmail.com>
2025-01-17 16:57:06 +0530

    Game 1 Completed

--- Product/Game1 Test.cs
+++ Product/Game1 Test.cs
@@ -6,38 +6,27 @@ namespace Product
     {
 
 
-        public int healcapacity { get; set; }
-        public string _name { get; set; }
+        public int HealCapacity { get; set; }
+        public string Name { get; set; }
         public int Health { get; set; }
         public int MaxHealth { get; set; }
         public int MeleeDamage { get; set; }
         public int RangeDamage { get; set; }
-
-        public int Defence { get; set; }
-
-        public List<string> SpecialAbilities { get; set; }
-        public List<string> Items { get; set; }
+        public string SpecialAbility { get; set; }
 
         public PlayerMain(string name, string abilty)    //Player class memebers
         {
             MaxHealth = 100;
-            _name = name;
+            Name = name;
             Health = 100;
             MeleeDamage = 40;
             RangeDamage = 30;
-            Defence = 10;
-            SpecialAbilities = new List<string>();
-            SpecialAbilities.Add(abilty);
-            Items = new List<string>();
-            healcapacity = 10;
-            Items.Add("Hammer");
-        }
 
+           
+            SpecialAbility=abilty;
 
-        public void AddItemToInventory(string item)    //add items 
-        {
-            Items.Add(item);
-            Console.WriteLine($"{_name} obtained {item}!");
+            HealCapacity = 10;
+
         }
 
         public void UpgradeStats()                   // upgrade player data
@@ -46,8 +35,7 @@ namespace Product
             Health = MaxHealth;
             MeleeDamage += 5;
             RangeDamage += 3;
-            Defence += 2;
-            healcapacity += 2;
+            HealCapacity += 2;
         }
 
 
@@ -59,24 +47,24 @@ namespace Product
             }
             else
             {
-                Health += healcapacity;
-                Console.WriteLine($"Healing Succesfull.New health is{Health}");
+                Health += HealCapacity;
+                Console.WriteLine($"Healing Succesfull.New health is {Health}");
             }
         }
 
 
-        public void takeDamage(int damage)   // player take damage
+        public void TakeDamage(int damage)   // player take damage
         {
 
             Health -= damage;
             if (Health <= 0)
             {
                 Health = 0;
-                Console.WriteLine($"{_name} took {damage} damage. Remaining health: {Health}");
+                Console.WriteLine($"{Name} took {damage} damage. Remaining health: {Health}");
             }
             else
             {
-                Console.WriteLine($"{_name} took {damage} damage. Remaining health: {Health}");
+                Console.WriteLine($"{Name} took {damage} damage. Remaining health: {Health}");
             }
         }
 
@@ -94,7 +82,7 @@ namespace Product
 
         public void details()
         {
-            Console.WriteLine($"PlayerName= {_name}\nHealth={Health}");   //player details
+            Console.WriteLine($"PlayerName= {Name}\nHealth={Health}");   //player details
         }
 
 
@@ -107,14 +95,14 @@ namespace Product
         public string Name { get; set; }
         public int Health { get; set; }
         public int MeleeDamage { get; set; }
-        public int Defence { get; set; }
+        public int Heal { get; set; }
 
-        public Enemy(string name, int health, int meleeDamage, int defence)
+        public Enemy(string name, int health, int meleeDamage, int heal)
         {
             Name = name;
             Health = health;
             MeleeDamage = meleeDamage;
-            Defence = defence;
+            Heal = heal;
         }
 
         public void takeDamage(int damage)
@@ -126,6 +114,7 @@ namespace Product
                 Health = 0;
                 Console.WriteLine($"{Name} took {damage} damage. Remaining health: {Health}");
                 Console.WriteLine($"{Name} died");
+
             }
             else
             {
@@ -134,6 +123,7 @@ namespace Product
 
 
         }
+
         public void details()
         {
             Console.WriteLine($"PlayerName= {Name}\nHealth={Health}");
@@ -164,13 +154,14 @@ namespace Product
 
         public void Start()             //Start fight here
         {
-            Console.WriteLine($"{player._name} embarks on a journey to reclaim the village from the Murlocs!");
+            Console.WriteLine($"{player.Name} embarks on a journey to reclaim the village from the Murlocs!");
 
 
             Console.WriteLine();
             Console.WriteLine("LOADING....");
             Console.WriteLine();
-            Thread.Sleep(5000);
+            Thread.Sleep(6000);
+            Console.Clear();
             //  Console.;
             while (currentLevel != 6)
             {
@@ -199,7 +190,7 @@ namespace Product
             int numberOfEnemies = currentLevel; // Level 1 ke pass 1 enemy and so on;
             for (int i = 0; i < numberOfEnemies; i++)
             {
-                enemies.Add(new Enemy($"Enemy {i + 1}", 50 + 10 * i, 10 + i * 2, 5 + i));
+                enemies.Add(new Enemy($"Enemy {i + 1}", 50 + 30 * i, 10 + i * 2, 8 + i));
             }
             Console.WriteLine();
             Console.WriteLine($"You will fight {numberOfEnemies} enemies.");
@@ -210,15 +201,17 @@ namespace Product
         private void BattleLoop()
         {
             bool levelComplete = false;
-            Random random = new Random();
-
+            // Random random = new Random();
+            int turn = 0;
             while (!levelComplete)
             {
 
-                int turn = random.Next(2);
+                // int turn = random.Next(2);
+
 
                 if (turn == 0)
                 {
+                    turn = 1;
                     Console.WriteLine();
                     Console.WriteLine("Your Turn");
 
@@ -229,6 +222,7 @@ namespace Product
                             PlayerTurn(enemy);
                             if (enemy.Health <= 0)
                             {
+                                enemies.Remove(enemy);
                                 break;
                             }
                         }
@@ -236,7 +230,7 @@ namespace Product
                 }
                 else  // 
                 {
-
+                    turn = 0;
                     //Console.WriteLine("Enemy Turn");
                     //  Console.WriteLine();
                     foreach (var enemy in enemies)
@@ -248,7 +242,7 @@ namespace Product
                             EnemyTurn(enemy);
                             if (player.Health <= 0)
                             {
-                                Console.WriteLine($"{player._name} has died. Game Over.");
+                                Console.WriteLine($"{player.Name} has died. Game Over.");
                                 break;
                             }
                         }
@@ -269,13 +263,26 @@ namespace Product
             Console.WriteLine("2. Heal");
             Console.WriteLine();
             string action = Console.ReadLine();
+            Random rand= new Random();
+            int ProbilityOfSpecialAbility=rand.Next(1,11);  //10 percent probalibity of activation
+
             if (action == "1")
             {
+                if(ProbilityOfSpecialAbility==5)
+                {
+                    Console.WriteLine($"Player Special Ability -> {player.SpecialAbility} Activated");
+                    player.Health = player.MaxHealth;
+                }
                 int damage = player.MeleeDamage;
                 enemy.takeDamage(damage);
             }
             else if (action == "2")
             {
+                if(ProbilityOfSpecialAbility == 5)
+                {
+                    Console.WriteLine($"Player Special Ability -> {player.SpecialAbility} Activated");
+                    player.Health = player.MaxHealth;
+                }
                 player.Heal();
             }
             else
@@ -294,7 +301,7 @@ namespace Product
             {
                 Console.WriteLine($"{enemy.Name} attacks!");
                 int damage = enemy.MeleeDamage;
-                player.takeDamage(damage);
+                player.TakeDamage(damage);
             }
             else
             {
@@ -310,7 +317,7 @@ namespace Product
             {
                 if (enemy.Health > 0)
                 {
-                    Console.WriteLine("Mission failed");
+
                     allEnemiesDefeated = false;
                     break;
                 }
@@ -319,9 +326,9 @@ namespace Product
             {
 
                 AwardLevelReward();
-                Console.WriteLine("WORKING---------");
-                // Thread.Sleep(3000);
-                /// Console.Clear();
+
+                Thread.Sleep(3000);
+                Console.Clear();
                 return true;
             }
             return false;
@@ -330,27 +337,33 @@ namespace Product
 
         private void AwardLevelReward()
         {
+            player.UpgradeStats();
+
             switch (currentLevel)
             {
                 case 1:
                     Console.WriteLine("Level 1 complete! You earned a Map.");
-                    player.UpgradeStats();
+                    Console.WriteLine("Next Level Loading");
                     break;
                 case 2:
                     Console.WriteLine("Level 2 complete! You earned a Sword (Melee Damage +5).");
                     player.MeleeDamage += 5;
+                    Console.WriteLine("Next Level Loading");
                     break;
                 case 3:
-                    Console.WriteLine("Level 3 complete! You earned a Shield (Defence +5).");
-                    player.Defence += 5;
+                    Console.WriteLine("Level 3 complete! You earned a Shield (HealCapacity +5).");
+                    player.HealCapacity += 5;
+                    Console.WriteLine("Next Level Loading");
                     break;
                 case 4:
-                    Console.WriteLine("Level 4 complete! You earned Armour (Defence +5).");
-                    player.Defence += 5;
+                    Console.WriteLine("Level 4 complete! You earned Armour (HealCapacity +5).");
+                    player.HealCapacity += 5;
+                    Console.WriteLine("Next Level Loading");
                     break;
                 case 5:
                     Console.WriteLine("Level 5 complete! You earned a Bow (Ranged Damage +5).");
                     player.RangeDamage += 5;
+                    Console.WriteLine("Boss Level Loading");
                     break;
                 case 6:
                     break;
@@ -374,6 +387,7 @@ namespace Product
             if (player.Health > 0)
             {
                 Console.WriteLine("You defeated Murlocs and reclaimed the village!");
+
             }
             else
             {
@@ -386,13 +400,14 @@ namespace Product
             Random rand = new Random();
             int move = rand.Next(1, 4);
 
+
             switch (move)
             {
                 case 1:
-                    Console.WriteLine("Murlocs uses Ground Slash! AOE damage!");
-                    int aoeDamage = 50;
-                    player.Health -= aoeDamage;
-                    Console.WriteLine($"You take {aoeDamage} damage. Remaining health: {player.Health}");
+                    Console.WriteLine("Murlocs uses Ground Slash! It damage you !");
+                    int aDamage = 50;
+                    player.Health -= aDamage;
+                    Console.WriteLine($"You take {aDamage} damage. Remaining health: {player.Health}");
                     break;
                 case 2:
                     Console.WriteLine("Murlocs uses Speed Dash! It bypasses your defence and strikes you!");
@@ -419,7 +434,7 @@ public class mainfield
 {
     public static void Main(string[] args)
     {
-        
+
 
         string line = "-----------------------------------";
         string message = "Welcome to Jatin Garg Gaming World";
@@ -441,9 +456,9 @@ public class mainfield
         Console.Write("Enter your hero's name: ");
         string playerName = Console.ReadLine();
 
-        // Console.Clear();
+        Console.Clear();
 
-        PlayerMain player = new PlayerMain(playerName, "Healing");
+        PlayerMain player = new PlayerMain(playerName, "Healing to MaxHealth");
 
 
         Game game = new Game(player);
